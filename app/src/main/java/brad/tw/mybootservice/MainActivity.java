5929b5c847c3d09d366803b51fc50563e065320a
Brad <brad@brad.tw>
2016-10-15 13:38:36 +0800

    v3

--- app/src/main/java/brad/tw/mybootservice/MainActivity.java
+++ app/src/main/java/brad/tw/mybootservice/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
@@ -37,12 +38,45 @@ public class MainActivity extends AppCompatActivity {
                     123);
         }
         tmgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        tmgr.listen(new MyPhoneStateListener(),
+                PhoneStateListener.LISTEN_CALL_STATE);
+
+
+    }
+
+    private class MyPhoneStateListener extends PhoneStateListener {
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            super.onCallStateChanged(state, incomingNumber);
+            switch (state){
+                case TelephonyManager.CALL_STATE_IDLE:
+                    Log.v("brad", "等候中");
+                    break;
+                case TelephonyManager.CALL_STATE_RINGING:
+                    Log.v("brad", "響鈴..." + incomingNumber);
+                    break;
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    Log.v("brad", "接通了");
+                    break;
+
+            }
+        }
+
+
+
 
     }
 
     private void queryPhoneInfo(){
         String lineNumber = tmgr.getLine1Number();
-        Log.v("brad", "==> " + lineNumber);
+        Log.v("brad", "LineNumber ==> " + lineNumber);
+        String imei = tmgr.getDeviceId();
+        Log.v("brad", "IMEI ==> " + imei);
+        String imsi = tmgr.getSubscriberId();
+        Log.v("brad", "IMSI ==> " + imsi);
+
+
+
     }
 
 
