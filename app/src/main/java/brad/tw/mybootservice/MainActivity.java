347af08b1d977630c1981972e8f3f4e9c1571efd
Brad <brad@brad.tw>
2016-10-15 13:02:55 +0800

    v2

--- app/src/main/java/brad/tw/mybootservice/MainActivity.java
+++ app/src/main/java/brad/tw/mybootservice/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,17 +25,30 @@ public class MainActivity extends AppCompatActivity {
                 != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_CONTACTS},
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    123);
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_SMS},
                     123);
         }
         tmgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
 
-
     }
 
+    private void queryPhoneInfo(){
+        String lineNumber = tmgr.getLine1Number();
+        Log.v("brad", "==> " + lineNumber);
+    }
+
+
     public void test1(View v){
-        Intent it = new Intent(this, MyBootService.class);
-        startService(it);
+        queryPhoneInfo();
+
     }
 
 }
